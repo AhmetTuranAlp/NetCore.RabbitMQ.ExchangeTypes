e138ff6d8b0dfe236adc474d93b23da187983900
Ahmet Turan Alp <32432160+AhmetTuranAlp@users.noreply.github.com>
2022-01-08 01:37:42 +0300

    Create Project
    First commit

---
+++ NetCore.RabbitMQ.ExchangeTypes.Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore.RabbitMQ.ExchangeTypes.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
